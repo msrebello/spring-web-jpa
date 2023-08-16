3f146027850c29437b82251eb489c0b0c44e51db
Matheus Rebello <matheussrb@outlook.com>
2023-08-16 17:03:00 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/matheusdev/springwebjpa/entities/OrderItem.java
+++ src/main/java/com/matheusdev/springwebjpa/entities/OrderItem.java
@@ -3,6 +3,7 @@ package com.matheusdev.springwebjpa.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.matheusdev.springwebjpa.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
@@ -31,6 +32,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
