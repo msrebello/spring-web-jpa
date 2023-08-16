ad4f7226ba89a0521b7937b247ef6e65b8745bc5
Matheus Rebello <matheussrb@outlook.com>
2023-08-16 17:16:02 -0300

    Payment, one-to-one association

--- src/main/java/com/matheusdev/springwebjpa/entities/Order.java
+++ src/main/java/com/matheusdev/springwebjpa/entities/Order.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import com.matheusdev.springwebjpa.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +35,9 @@ public class Order {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -78,7 +83,15 @@ public class Order {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
